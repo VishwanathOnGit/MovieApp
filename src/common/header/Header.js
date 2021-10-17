d7c64ba5e2104527bd44f748ed5bbc2844000f7d
Vishwanath Pucha <putchavishwanath@gmail.com>
2021-10-17 23:17:34 +0530

    Registration module added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -23,35 +23,113 @@ function Error() {
 
 const Header = function (props) {
 
-    const [addSubscriberForm, setAddSubscriberForm] = useState({
+    const [addUserForm, setAddUserForm] = useState({
         id: 0,
-        firstname: '',
-        lastname: '',
-        phone: '',
+        first_name: '',
+        last_name: '',
+        email_address: '',
+        password: '',
+        mobile_number: '',
         errors: {
-            firstname: '',
-            lastname: '',
-            phone: '',
+            first_name: '',
+            last_name: '',
+            email_address: '',
+            password: '',
+            mobile_number: '',
         }
     });
+    const [addUserFormMessage, setAddUserFormMessage] = useState("");
+
+    async function addUserHandler(newUser) {
+
+        const rawResponse = await fetch("http://localhost:8085/api/v1/signup",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            }
+        );
+
+        const data = await rawResponse.json();
+
+        if (data.status === "ACTIVE") {
+            setAddUserFormMessage("Registration Successful. Please Login!");
+        } else {
+            setAddUserFormMessage(data.message);
+        }
+    }
+
+    function checkEmptyValue(value) {
+        return (value.length == 0)
+            ? 'required'
+            : '';
+    }
 
     const inputChangedHandler = (e) => {
+        const state = addUserForm;
+        const {name, value} = e.target;
+        let errors = state.errors;
 
-        const state = addSubscriberForm;
+        switch (name) {
+            case 'first_name':
+                errors.first_name = checkEmptyValue(value);
+                break;
+            case 'last_name':
+                errors.last_name = checkEmptyValue(value);
+                break;
+            case 'mobile_number':
+                errors.mobile_number = checkEmptyValue(value);
+                break;
+            case 'email_address':
+                errors.email_address = checkEmptyValue(value);
+                break;
+            case 'password':
+                errors.password = checkEmptyValue(value);
+                break;
+            default:
+                break;
+        }
+
         state[e.target.name] = e.target.value;
-        console.log(state)
-        setAddSubscriberForm({...state});
+        setAddUserForm({...state});
     }
 
-    const onFormSubmitted = (e) => {
+    const onRegisterFormSubmitted = (e) => {
         e.preventDefault();
+        const state = addUserForm;
+        let errors = state.errors;
+        // On submit validation
+        if (state.first_name.length === 0) {
+            errors.first_name = "required";
+        }
+        if (state.last_name.length === 0) {
+            errors.last_name = "required";
+        }
+        if (state.email_address.length === 0) {
+            errors.email_address = "required";
+        }
+        if (state.password.length === 0) {
+            errors.password = "required";
+        }
+        if (state.mobile_number.length === 0) {
+            errors.mobile_number = "required";
+        }
 
-        // addSubscriberHandler(addSubscriberForm);
-        setAddSubscriberForm({id: 0, firstname: '', phone: '', lastname: ''});
-        // history.push("/")
+        // set state if errors found
+        if (state.errors.first_name.length > 0 ||
+            state.errors.last_name.length > 0 ||
+            state.errors.email_address.length > 0 ||
+            state.errors.password.length > 0 ||
+            state.errors.mobile_number.length > 0) {
+            setAddUserForm({...state});
+        } else {
+            addUserHandler(addUserForm);
+        }
     }
 
-    const {firstname, phone} = addSubscriberForm;
+    const {first_name, last_name, email_address, password, mobile_number} = addUserForm;
 
     const [modalShow, setModalShow] = useState(false);
     const [value, setValue] = React.useState(1);
@@ -80,7 +158,7 @@ const Header = function (props) {
                 </div>
             </div>
             <Modal
-                isOpen={modalShow}
+                isOpen={true}
                 shouldCloseOnOverlayClick={false}
                 onRequestClose={() => setModalShow(false)}
                 ariaHideApp={false}
@@ -132,7 +210,7 @@ const Header = function (props) {
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    <form onSubmit={onFormSubmitted}>
+                    <form onSubmit={onRegisterFormSubmitted}>
                         <Grid container direction="column" alignItems="center" justify="center">
                             <FormControl
                                 style={{
@@ -140,64 +218,96 @@ const Header = function (props) {
                                 }}
                             >
                                 <InputLabel
-                                    htmlFor="firstname"
+                                    htmlFor="first_name"
                                     required
                                 >First Name</InputLabel>
                                 <Input
-                                    id="firstname"
+                                    id="first_name"
                                     className="form-field"
                                     type="text"
-                                    name="firstname"
+                                    name="first_name"
                                     onChange={inputChangedHandler}
-                                    value={firstname}
+                                    value={first_name}
                                 />
-                                <Error >required</Error>
+                                {addUserForm.errors.first_name.length > 0 &&
+                                <Error>{addUserForm.errors.first_name}</Error>}
                             </FormControl>
-                            {/*<FormControl*/}
-                            {/*    className="form-field"*/}
-                            {/*    variant="standard"*/}
-                            {/*    style={{*/}
-                            {/*        marginTop: "20px"*/}
-                            {/*    }}*/}
-                            {/*>*/}
-                            {/*    <InputLabel htmlFor="lastname">Last Name *</InputLabel>*/}
-                            {/*    <Input id="lastname"/>*/}
-                            {/*</FormControl>*/}
-                            {/*<FormControl*/}
-                            {/*    className="form-field"*/}
-                            {/*    variant="standard"*/}
-                            {/*    style={{*/}
-                            {/*        marginTop: "20px"*/}
-                            {/*    }}*/}
-                            {/*>*/}
-                            {/*    <InputLabel htmlFor="email">Email *</InputLabel>*/}
-                            {/*    <Input id="email"/>*/}
-                            {/*</FormControl>*/}
-                            {/*<FormControl*/}
-                            {/*    className="form-field"*/}
-                            {/*    variant="standard"*/}
-                            {/*    style={{*/}
-                            {/*        marginTop: "20px"*/}
-                            {/*    }}>*/}
-                            {/*    <InputLabel htmlFor="password">Password *</InputLabel>*/}
-                            {/*    <Input id="password"/>*/}
-                            {/*</FormControl>*/}
-                            {/*<FormControl*/}
-                            {/*    className="form-field"*/}
-                            {/*    variant="standard"*/}
-                            {/*    style={{*/}
-                            {/*        marginTop: "20px"*/}
-                            {/*    }}>*/}
-                            {/*    <InputLabel htmlFor="contact">Contact No *</InputLabel>*/}
-                            {/*    <Input id="contact"/>*/}
-                            {/*</FormControl>*/}
+                            <FormControl
+                                style={{
+                                    marginTop: "20px"
+                                }}
+                            >
+                                <InputLabel htmlFor="last_name">Last Name *</InputLabel>
+                                <Input
+                                    id="last_name"
+                                    className="form-field"
+                                    type="text"
+                                    name="last_name"
+                                    onChange={inputChangedHandler}
+                                    value={last_name}/>
+                                {addUserForm.errors.last_name.length > 0 &&
+                                <Error>{addUserForm.errors.last_name}</Error>}
+                            </FormControl>
+                            <FormControl
+                                style={{
+                                    marginTop: "20px"
+                                }}
+                            >
+                                <InputLabel htmlFor="email_address">Email *</InputLabel>
+                                <Input
+                                    id="email_address"
+                                    className="form-field"
+                                    type="email"
+                                    name="email_address"
+                                    onChange={inputChangedHandler}
+                                    value={email_address}/>
+                                {addUserForm.errors.email_address.length > 0 &&
+                                <Error>{addUserForm.errors.email_address}</Error>}
+                            </FormControl>
+                            <FormControl
+                                style={{
+                                    marginTop: "20px"
+                                }}>
+                                <InputLabel htmlFor="password">Password *</InputLabel>
+                                <Input
+                                    id="password"
+                                    className="form-field"
+                                    type="password"
+                                    name="password"
+                                    onChange={inputChangedHandler}
+                                    value={password}/>
+                                {addUserForm.errors.password.length > 0 &&
+                                <Error>{addUserForm.errors.password}</Error>}
+                            </FormControl>
+                            <FormControl
+                                style={{
+                                    marginTop: "20px"
+                                }}>
+                                <InputLabel htmlFor="mobile_number">Contact No *</InputLabel>
+                                <Input
+                                    id="mobile_number"
+                                    className="form-field"
+                                    type="text"
+                                    name="mobile_number"
+                                    onChange={inputChangedHandler}
+                                    value={mobile_number}/>
+                                {addUserForm.errors.mobile_number.length > 0 &&
+                                <Error>{addUserForm.errors.mobile_number}</Error>}
+                            </FormControl>
+
+                            {addUserFormMessage.length > 0 &&
+                            <Typography
+                                style={{
+                                    marginTop: "30px",
+                                    fontSize: "16px"
+                                }}>{addUserFormMessage}</Typography>}
                             <Button
                                 type="submit"
                                 variant="contained"
                                 color="primary"
                                 size="large"
                                 style={{
-                                    marginTop: "40px"
+                                    marginTop: "30px"
                                 }}
                             >REGISTER</Button>
                         </Grid>
